f64965e440a93401ce44dcd2da34c99bc21fb0b5
Lisa Bronwyn <e.bronwyn@gmail.com>
2016-12-13 11:16:31 -0800

    added title author link

--- data/library.js
+++ data/library.js
@@ -5,9 +5,57 @@ const library = [
     description:""
   },
   {
-    title: "",
-    author:"",
-    bookUrl:"",
+    title: "All The Birds, Singing",
+    author:"Evie Wyld",
+    bookUrl:"images/book-cover-images/allthebirdssinging-1536x2320.jpg",
+    description:""
+  },
+  {
+    title: "An Object Of Beauty",
+    author:"Steve Martin",
+    bookUrl:"images/book-cover-images/anobjectofbeatury-1536x2273.jpg",
+    description:""
+  },
+  {
+    title: "Area X",
+    author:"Jeff VanderMeer",
+    bookUrl:"images/book-cover-images/areax-1536x2043.jpg",
+    description:""
+  },
+  {
+    title: "Bad Teeth",
+    author:"Justin Long",
+    bookUrl:"images/book-cover-images/Bad-Teeth-Dustin-Long-1536x2302.jpg",
+    description:""
+  },
+  {
+    title: "The Book Of Dead Philosophers",
+    author:"Simon Critchley",
+    bookUrl:"images/book-cover-images/book-of-dead-philosophers-1536x2760.png",
+    description:""
+  },
+  {
+    title: "Breathing Room",
+    author:"Peter Davison",
+    bookUrl:"images/book-cover-images/breathingroom-1536x2233.jpg",
+    description:""
+  },
+  {
+    title: "Burn This Book",
+    author:"Toni Morrison (ed.)",
+    bookUrl:"images/book-cover-images/breathingroom-1536x2233.jpg",
+    description:"images/book-cover-images/burn-this-book-1536x2306.jpg"
+  },
+  {
+    title: "Columbine",
+    author:"Dave Cullen",
+    bookUrl:"images/book-cover-images/columbine-1536x2370.jpg",
+    description:""
+  },
+  {
+    title: "The Curious Incident Of The Dog In The Night-time",
+    author:"Mark Hadden",
+    bookUrl:"images/book-cover-images/curious-incident-of-the-dog-1536x2358.png",
     description:""
   },
   {
@@ -21,53 +69,5 @@ const library = [
     author:"",
     bookUrl:"",
     description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  },
-  {
-    title: "",
-    author:"",
-    bookUrl:"",
-    description:""
-  }  
+  }
 ];
