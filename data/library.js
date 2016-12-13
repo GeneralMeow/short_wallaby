d752debf610d24c49deff87297939cc814577e0b
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-12-13 10:28:11 -0800

    add data folder and files for library

---
+++ data/library.js
@@ -0,0 +1,73 @@
+const library = [
+  {
+    title: "1984",
+    author:"George Orwell", bookUrl:"images/book-cover-images2/1984-1536x2028.jpg",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  },
+  {
+    title: "",
+    author:"",
+    bookUrl:"",
+    description:""
+  }  
+];
