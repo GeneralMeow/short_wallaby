57e470c3ecd84a118da5dce69bfe119db8f0168b
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-12-15 11:57:56 -0800

    adds pug templating and functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var pug = require('pug');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//TODO:Are we already serving public folders ^ or do we need below line?
+// app.use('/static', express.static('public'))
 
 app.use('/', index);
 app.use('/users', users);
