7403abf15f7e86ed9b5a583f62e639966635ccbb
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-12-14 16:31:01 -0800

    adding backend

--- database.js
+++ database.js
@@ -4,10 +4,14 @@ const connectionString = process.env.DATABASE_URL || `postgres://${process.env.U
 const pgp = require('pg-promise')()
 const database = pgp(connectionString)
 
+const getAllBooks = () => {
+  const sql = 'SELECT * FROM books'
+  return database.any(sql)
+};
 
 
 
 
 module.exports = {
-
+  getAllBooks
 }
