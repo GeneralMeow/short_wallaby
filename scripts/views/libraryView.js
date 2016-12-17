e620051f46d59ad1ced0a03392f4d606e1d17a88
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-12-17 14:06:34 -0800

    added css for book add and edit panels

--- scripts/views/libraryView.js
+++ scripts/views/libraryView.js
@@ -18,6 +18,7 @@ libraryView.getBookId = function() {
     $('img').on('click', function() {
       img = $(this).attr('src');
       $('.tab-content').hide();
+      $('#add-book-bottom').hide();
       $('#book-detail').show();
         Library.allBooks.forEach(function(book) {
           if(book.bookUrl === img) {
@@ -26,10 +27,14 @@ libraryView.getBookId = function() {
             $('.book-title').html(book.title);
             $('.book-author').html(book.author);
             $('.book-description').html(book.description);
-            return bookIndex;
           }
+
         })
+        libraryView.bookDelete(bookIndex);
+        libraryView.renderBookEdit();
     })
+
+
 };
 
 libraryView.renderBookDetail = function() {
@@ -37,33 +42,48 @@ libraryView.renderBookDetail = function() {
 };
 
 libraryView.renderBookEdit = function() {
+  $('#edit-book').on('click', function(event) {
+    event.preventDefault();
+    $('.tab-content').hide();
+    $('#add-book-bottom').hide();
 
+    $('#book-edit').show();
+  })
+}
+
+libraryView.bookDelete = function(bookIndex) {
+  //delete this clicked book from array
+  $('#delete-book').on('click', function(event) {
+    event.preventDefault();
+    // console.log("in bookDelete");
+    // console.log('bookIndex', bookIndex);
+    //TODO: re-load sliced array of allBooks into local storage
+    // Library.allBooks.splice(bookIndex, 1);
+    $('.tab-content').hide();
+    $('#add-book-bottom').hide();
+    $('#home').show();
+  });
 };
 
 libraryView.renderBookAdd = function() {
-
-};
-
-
-//event handlers
-$(document).ready(function() {
   $('#add-book').on('click', function(event) {
     event.preventDefault();
     $('.book-holder-image').attr('src', 'http://placehold.it/170x260.jpg');
     $('.tab-content').hide();
+    $('#add-book-bottom').hide();
     $('#book-add').show();
   })
+};
 
-  $('#edit-book').on('click', function(event) {
-    event.preventDefault();
-    $('.tab-content').hide();
-    $('#book-edit').show();
-  })
-
+libraryView.returnHome = function() {
   $('#logo').on('click', function() {
     $('.tab-content').hide();
+    $('#add-book-bottom').hide();
     $('#home').show();
   })
-});
+}
 
 libraryView.getBookId();
+libraryView.renderBookAdd();
+libraryView.returnHome();
+libraryView.bookDelete();
