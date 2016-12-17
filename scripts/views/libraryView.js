2e347bc7eab5fec61396a54527b1cb8537e0aa8c
Rose Hip <rosehip@Roses-MacBook-Pro.local>
2016-12-17 11:47:58 -0800

    adds front-end functionality

--- scripts/views/libraryView.js
+++ scripts/views/libraryView.js
@@ -1,30 +1,55 @@
+'use strict';
+var libraryView = {};
 //functions
+// libraryView.renderHome = function() {
+//   Library.fetchAll();
+//   library.forEach(function(book) {
+//     //needs const
+//     const htmlString = '<div class="grid-item"><img src="' + book.bookUrl + '"></div>';
+//     // console.log(htmlString);
+//     //append each book to the book grid
+//     $('.grid').append(htmlString);
+//   });
+// };
 
-let img;
-let bookIndex;
-function selectBook() {
-  $('img').on('click', function() {
-    img = $(this).attr('src');
-    $('.tab-content').hide();
-    $('#book-detail').show();
-      Library.allBooks.forEach(function(book) {
-        if(book.bookUrl === img) {
-          bookIndex = Library.allBooks.indexOf(book)
-          $('img').attr('src', book.bookUrl);
-          $('.book-title').html(book.title);
-          $('.book-author').html(book.author);
-          $('.book-description').html(book.description);
-          return bookIndex;
-        }
-      })
-  })
+libraryView.getBookId = function() {
+  let img;
+  let bookIndex;
+    $('img').on('click', function() {
+      img = $(this).attr('src');
+      $('.tab-content').hide();
+      $('#book-detail').show();
+        Library.allBooks.forEach(function(book) {
+          if(book.bookUrl === img) {
+            bookIndex = Library.allBooks.indexOf(book)
+            $('.book-holder-image').attr('src', book.bookUrl);
+            $('.book-title').html(book.title);
+            $('.book-author').html(book.author);
+            $('.book-description').html(book.description);
+            return bookIndex;
+          }
+        })
+    })
+};
 
-}
+libraryView.renderBookDetail = function() {
+
+};
+
+libraryView.renderBookEdit = function() {
+
+};
+
+libraryView.renderBookAdd = function() {
+
+};
+
 
 //event handlers
 $(document).ready(function() {
   $('#add-book').on('click', function(event) {
     event.preventDefault();
+    $('.book-holder-image').attr('src', 'http://placehold.it/170x260.jpg');
     $('.tab-content').hide();
     $('#book-add').show();
   })
@@ -41,4 +66,4 @@ $(document).ready(function() {
   })
 });
 
-selectBook();
+libraryView.getBookId();
